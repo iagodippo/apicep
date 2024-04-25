d3ab1c6e40cb76fee88acfb07a82455fc03865cf
Iago D'Ippolito <iagodippolito@hotmail.com>
2024-04-25 16:43:03 -0300

    primeiro

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const pool = require('./db')
+const port = 3000
+
+const app = express()
+app.set('trust proxy', true)
+
+const ipWhitelistMiddleware = async (req, res, next) => {
+    const clientIp = req.ip;
+    console.log(`IP USER: ${clientIp}`)
+    
+    try {
+      const result = await pool.query('SELECT ip_address FROM whitelist_ips');
+      const ipAddresses = result.rows.map(row => row.ip_address);
+      
+      if (ipAddresses.includes(clientIp)) {
+        next();
+      } else {
+        res.status(403).send('Acesso não autorizado');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar IPs na whitelist:', error);
+      res.status(500).send('Erro interno do servidor');
+    }
+  };
+
+// Endpoint para busca de CEP
+app.get('/cep/:id', ipWhitelistMiddleware, async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const result = await pool.query('SELECT * FROM ceps WHERE cep = $1', [id]);
+      const cep = result.rows[0];
+      if (cep) {
+        res.json(cep);
+      } else {
+        res.status(404).send('CEP não encontrado');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar CEP:', error);
+      res.status(500).send('Erro interno do servidor');
+    }
+  });
+
+app.listen(port, () => console.log(`Serviço startado na porta: ${port}`))
